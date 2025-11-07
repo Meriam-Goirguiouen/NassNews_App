a359306670bb85076120d8dfa52c579a83358aee
Meriam-Goirguiouen <meriam.goirguiouen2002@gmail.com>
2025-11-07 16:04:57 +0100

    pousser la version actuelle de code - solution urgente

--- backend/backend/src/test/java/ma/nassnewsapp/backend/controller/EvenementControllerTest.java
+++ backend/backend/src/test/java/ma/nassnewsapp/backend/controller/EvenementControllerTest.java
@@ -1,6 +1,7 @@
 package ma.nassnewsapp.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ma.nassnewsapp.backend.entities.Evenement;
 import ma.nassnewsapp.backend.services.EvenementService;
 import org.junit.jupiter.api.Test;
