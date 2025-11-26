6b4eb20af3249f7c3b20a8b8418cfc2a735d7921
Meriam-Goirguiouen <meriam.goirguiouen2002@gmail.com>
2025-11-26 04:28:11 +0100

    Feature: VilleService complet avec détection par IP et coordonnées via API externe

--- frontend/nassnews-frontend/src/App.vue
+++ frontend/nassnews-frontend/src/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import Navbar from './components/Navbar.vue'
 import HeroCarousel from './components/HeroCarousel.vue'
 import AboutSection from './components/AboutSection.vue'
 import ContactSection from './components/ContactSection.vue'
 import Footer from './components/Footer.vue'
+
+import { getPosition } from "./services/geolocation";
+
+// Variables pour stocker latitude et longitude
+const latitude = ref<number | null>(null)
+const longitude = ref<number | null>(null)
+
+onMounted(() => {
+  getPosition()
+    .then(pos => {
+      latitude.value = pos.coords.latitude
+      longitude.value = pos.coords.longitude
+      console.log("Latitude:", latitude.value, "Longitude:", longitude.value)
+    })
+    .catch(err => console.error("Erreur géolocalisation:", err))
+})
 </script>
 
 <template>
